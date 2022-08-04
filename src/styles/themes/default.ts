c23574ce7cfaaebad6fc15e8dba9dd16999376ad
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-08-04 07:02:26 -0300

    style: update colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -11,9 +11,9 @@ export const defaultTheme = {
   "base-subtitle": "#403937",
   "base-text": "#574F4D",
   "base-label": "#8D8686",
-  "base-hover": "#8D8686",
-  "base-button": "#8D8686",
-  "base-input": "#8D8686",
+  "base-hover": "#D7D5D5",
+  "base-button": "#E6E5E5",
+  "base-input": "#E6E5E5",
   "base-card": "#F3F2F2",
 
   bg: "#FAFAFA",
