25303ae17f313eedc57f926c06777f90dd465e52
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-08-24 07:10:41 -0300

    feat: create section payment methods

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,13 +2,74 @@ import {
   CheckoutContainer,
   CoffeesSelectedContainer,
   DeliveryFormContainer,
+  BaseContainerCheckout,
+  BaseContainerDescription,
+  RadioButtonsContainer,
+  RadioButton,
 } from "./styles";
 
+import MapOutlined from "../../assets/icons/map-outlined.svg";
+import MoneyOutlined from "../../assets/icons/money-outlined.svg";
+import CardCredit from "../../assets/icons/card.svg";
+import CardDebit from "../../assets/icons/card-debit.svg";
+import Dollar from "../../assets/icons/dollar.svg";
+import { useState } from "react";
+
+type PaymentMethod = "credit" | "debit" | "money";
+
 export function Checkout() {
+  const [selected, setSelected] = useState<PaymentMethod>();
+
   return (
     <CheckoutContainer>
       <DeliveryFormContainer>
         <h3>Complete seu pedido</h3>
+        <BaseContainerCheckout>
+          <BaseContainerDescription>
+            <img src={MapOutlined} width={22} height={22} alt="" />
+            <div>
+              <h4>Endereço de entrega</h4>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </BaseContainerDescription>
+        </BaseContainerCheckout>
+
+        <BaseContainerCheckout>
+          <BaseContainerDescription>
+            <img src={MoneyOutlined} width={22} height={22} alt="" />
+            <div>
+              <h4>Pagamento</h4>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </BaseContainerDescription>
+          <RadioButtonsContainer>
+            <RadioButton
+              onClick={() => setSelected("credit")}
+              selected={selected === "credit"}
+            >
+              <img width={20} height={20} src={CardCredit} alt="" />
+              Cartão de crédito
+            </RadioButton>
+
+            <RadioButton
+              onClick={() => setSelected("debit")}
+              selected={selected === "debit"}
+            >
+              <img width={20} height={20} src={CardDebit} alt="" />
+              Cartão de débito
+            </RadioButton>
+
+            <RadioButton
+              onClick={() => setSelected("money")}
+              selected={selected === "money"}
+            >
+              <img width={20} height={20} src={Dollar} alt="" />
+              Dinheiro
+            </RadioButton>
+          </RadioButtonsContainer>
+        </BaseContainerCheckout>
       </DeliveryFormContainer>
 
       <CoffeesSelectedContainer>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -17,6 +17,75 @@ export const DeliveryFormContainer = styled.div`
   }
 `;
 
+export const BaseContainerCheckout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2.5rem;
+  background-color: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+`;
+
+export const BaseContainerDescription = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 8px;
+
+  h4 {
+    font-size: 1rem;
+    font-family: "Roboto";
+    font-weight: 400;
+    line-height: 130%;
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+
+  p {
+    font-size: 0.875rem;
+    font-family: "Roboto";
+    font-weight: 400;
+    color: ${(props) => props.theme["base-text"]};
+    line-height: 130%;
+  }
+`;
+
+export const RadioButtonsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 12px;
+`;
+
+interface ButtonSelectedProps {
+  selected: boolean | undefined;
+}
+
+export const RadioButton = styled.button<ButtonSelectedProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  gap: 12px;
+  min-width: 178px;
+  cursor: pointer;
+  height: 51px;
+  border-radius: 6px;
+  padding: 1rem;
+  font-size: 0.75rem;
+  font-family: "Roboto";
+  color: ${(props) => props.theme["base-text"]};
+  text-transform: uppercase;
+  background-color: ${(props) =>
+    props.selected ? props.theme["purple-light"] : props.theme["base-button"]};
+  border: ${(props) =>
+    props.selected
+      ? `1px solid ${props.theme.purple}`
+      : " 1px solid transparent"};
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: ${(props) => props.theme["base-hover"]};
+  }
+`;
+
 export const CoffeesSelectedContainer = styled.div`
   display: flex;
   flex-direction: column;
